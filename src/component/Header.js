3a8e2f9725d0fa516ab4281cc7a907307fda630e
Yebin Ha <beanswebdev@gmail.com>
2022-12-08 10:51:59 +0900

    Header hover effect 추가

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,34 @@
 import {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
+import './Header.css';
 
 
 function Header() {
     const [style, setStyle] = useState({width:'100%', position:'fixed', left:0, top:0, zIndex:10});
+
+    const hoverSearch = (e) => {
+        e.target.src = 'btn-search-k.png';
+    } 
+
+    const unhoverSearch = (e) => {
+        e.target.src = 'btn-search.png';
+    } 
+
+    const hoverCoco = (e) => {
+        e.target.src = 'btn-coco-k.png';
+    } 
+
+    const unhoverCoco = (e) => {
+        e.target.src = 'btn-coco.png';
+    } 
+
+    const hoverQ = (e) => {
+        e.target.src = 'btn-question-k.png';
+    } 
+
+    const unhoverQ = (e) => {
+        e.target.src = 'btn-question.png';
+    } 
 
     return(
         <header style={style} id='nav-header' >
@@ -14,15 +39,15 @@ function Header() {
 
                 <div className='nav-pageitems'>
                     <Link to={'/search'} className='nav-item'> 
-                        <img src='btn-search.png' alt=''/>
+                        <img src='btn-search.png' alt='' onMouseOver={hoverSearch} onMouseOut={unhoverSearch}/>
                     </Link>
 
                     <Link to={'/cocoform'} className='nav-item'> 
-                        <img src='btn-coco.png' alt=''/>
+                        <img src='btn-coco.png' alt='' onMouseOver={hoverCoco} onMouseOut={unhoverCoco}/>
                     </Link>
 
                     <Link to={'/question'} className='nav-item'> 
-                        <img src='btn-question.png' alt=''/>
+                        <img src='btn-question.png' alt='' onMouseOver={hoverQ} onMouseOut={unhoverQ}/>
                     </Link>
                 </div>
 
@@ -33,5 +58,13 @@ function Header() {
         </header>
     )
 }
+
+// function hover(element){
+//     element.setAttribute('src', 'btn-search-k.png');
+// }
+
+// function unhover(element){
+//     element.setAttribute('src', 'btn-home.png');
+// }
 
 export default Header;
