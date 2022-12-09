7d407a3f53f5e8458129d92f4f88836e90548599
bang3821 <01066303821jh@gmail.com>
2022-12-09 21:20:03 +0900

    로그인 & 회원가입 구현

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import {Routes, Route} from 'react-router-dom';
-
+import { Routes, Route } from 'react-router-dom';
+import {useSelector} from "react-redux";
 import Header from './component/Header';
-// import Main_cocoList from './component/Main_cocoList';
-import MainCoco from './component/MainCoco';
+import Signup from './component/Signup';
+import Login from './component/Login';
+
 
 function App() {
+  const token = useSelector((state) => state.Auth.token);
+  console.log(token);
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
-        {/* <Route exact path='/' element={<Main_cocoList/>}/> */}
-        {/* <Route exact path='/' element={<MainCoco/>}/> */}
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/signup' element={<Signup />} />
       </Routes>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import store from "./redux/configStore";
+import { PersistGate } from "redux-persist/integration/react";
+import persistStore from "redux-persist/es/persistStore";
+
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
