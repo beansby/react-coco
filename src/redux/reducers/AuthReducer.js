e567f1ca68a159f59ef0e3103282fbc6322ff813
Yebin Ha <beanswebdev@gmail.com>
2022-12-14 19:41:05 +0900

    미사용

--- src/redux/reducers/AuthReducer.js
+++ src/redux/reducers/AuthReducer.js
@@ -1,17 +1,18 @@
-const SET_TOKEN = 'set_token';
 
+// 로그인 전
 const AuthInitialState = {
     token: null
 }
 
+// 로그인 : setToken 호출
 export const setToken = (token) => ({
-    type: SET_TOKEN,
+    type: "SET_TOKEN",
     token
 })
 
 export const AuthReducer = (state = AuthInitialState, action) => {
     switch (action.type) {
-        case SET_TOKEN:
+        case "SET_TOKEN" :
             return {
                 ...state,
                 token: action.token
