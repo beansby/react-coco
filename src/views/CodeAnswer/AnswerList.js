3ede6a5f82f13600ecb11911f2b37fe901ccb522
bang3821 <01066303821jh@gmail.com>
2022-12-28 17:02:26 +0900

    answer delete

--- src/views/CodeAnswer/AnswerList.js
+++ src/views/CodeAnswer/AnswerList.js
@@ -12,6 +12,7 @@ import "react-confirm-alert/src/react-confirm-alert.css";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import { Editor } from "@toast-ui/react-editor";
 import { Viewer } from "@toast-ui/react-editor";
+import ScrollToTop from "../../components/ScrollToTop";
 
 function AnswerList() {
 	// 토큰 보내기 시작
@@ -21,6 +22,7 @@ function AnswerList() {
 
 	const [member, setMember] = useState({});
 	const [cookie, setCookie] = useCookies([]);
+	const [answerId, setAnswerId] = useState('');
 
 	const requestUser = async () => {
 		try {
@@ -69,6 +71,7 @@ function AnswerList() {
 			.then((response) => {
 				setAnswers([...answers, response.data]);
 				console.log("답변 등록 성공");
+				console.log(response.data);
 				// 질문 등록 후 질문 리스트 or 질문 상세 페이지 이동
 			})
 			.catch((err) => {
@@ -103,8 +106,10 @@ function AnswerList() {
 			.get(`http://localhost:8080/api/questions/${id}/answers`)
 			.then((res) => {
 				const answers = res.data;
-				console.log(answers);
+				// console.log(answers);
 				setAnswers([...answers]);
+				console.log('댓글 가져오기 성공');
+				console.log(answers);
 			})
 			.catch((error) => {
 				console.log(error);
@@ -115,37 +120,34 @@ function AnswerList() {
 		fetchData();
 	}, []);
 
-	// 답글 삭제 수정 요망
-	const deleteBtn = (answerId) => {
-		axios.delete(`http://localhost:8080/api/answers/${answerId}/delete/`).then(
-			((response) => {
-				setAnswers(answers.filter((answer) => answer.id !== answerId));
-				const msg = response.data;
-				alert(msg);
-			}).catch((error) => {
-				console.log(error);
-			})
-		);
-	};
 
-	// 질문 삭제
-	const deleteQuestion = (e) => {
+	// 댓글 삭제
+	const deleteAnswer = (id) => {
+
 		axios
-			.delete(`http://localhost:8080/api/answers/${id}`, null, {
-				headers: { Authorization: token },
-			})
-			.then((res) => {
+			.delete(`http://localhost:8080/api/answers/${id}`
+			).then((res) => {
+				// setAnswers(answers.filter((answer)=> answer.id !== id));
+				console.log(res.data);
+				console.log('댓글 삭제 성공');
+				console.log(answers);
+				fetchData();
+				
 				alert("삭제가 완료되었습니다.");
-				document. location.href = "/question";
+				
+				const reUrl = answers[0].question.questionId;
+				document. location.href = "/question/"+reUrl;
 			})
 			.catch((error) => {
 				console.log(error);
 			});
 	};
 
-	// 질문 삭제 Alert
+	
+	// 댓글 삭제 Alert
 	const deleteConfirm = (e) => {
 		e.preventDefault();
+		console.log(e.target.id);
 		confirmAlert({
 			title: "삭제 하시겠습니까?",
 			message: "뒤로 돌아가려면 취소를 눌러주세요.",
@@ -153,7 +155,7 @@ function AnswerList() {
 				{
 					label: "확인",
 					onClick: () => {
-						deleteQuestion();
+						deleteAnswer(e.target.id);
 					},
 				},
 				{
@@ -164,27 +166,13 @@ function AnswerList() {
 		});
 	};
 
-	// const LikeButton = () => {
-	// 	const [likes, setLikes] = useState(100);
-	// 	const [isClicked, setIsClicked] = useState(false);
-
-	// 	const handleClick = () => {
-	// 		if (isClicked) {
-	// 			setLikes(likes - 1);
-	// 		} else {
-	// 			setLikes(likes + 1);
-	// 		}
-	// 		setIsClicked(!isClicked);
-	// 	};
-	// }
-
 		return (
 			<section>
 				<Form className="a-list-form-container">
 					<div className="folder-content-answer">
 						{answers.map((answers) => {
 							return (
-								<div className="row a-detail-info">
+								<div className="row a-detail-info" key={answers.answerId}>
 									<div classname="col-8">
 										<img src="" alt="" />
 										<span className="a-user-nickname">
@@ -199,24 +187,26 @@ function AnswerList() {
 											<Viewer initialValue={answers.content} />
 										</div>
 									</div>
-									<Link
-										to={"/answer/" + answers.answerId + "/modify"}
-										key={answers.answerId}
-									>
-										<button>수정</button>
-									</Link>
-									<span>
-										<button onClick={deleteConfirm}>삭제</button>
-									</span>
+									
+									{memberId == answers.answerAuthor.email && (
+										<div>
+										<Link
+											to={"/answer/" + answers.answerId + "/modify"}
+											// key={answers.answerId}
+										>
+											<button>수정</button>
+										</Link>
+										<span>
+											{/* 댓글 삭제 */}
+											<button id={answers.answerId} onClick={deleteConfirm}>삭제</button>
+										</span>
+										</div>
+									)}
 								</div>
 							);
 						})}
 					</div>
-					{/* <span className="like-btn">
-						<button className={`like-button ${isClicked && 'liked'}`} onClick={handleClick}>
-							<span className="likes-counter">{`Like | ${likes}`}</span>
-						</button>
-					</span> */}
+
 				</Form>
 				<Form className="a-detail-form-container">
 					<Editor
